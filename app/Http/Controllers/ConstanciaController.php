af2d41d90962cf9490a75150217df7352ce5c031
r3drum <mauriciomorales0410@gmail.com>
2024-05-02 09:22:32 -0400

    update form constancias add rehab field minors ux changes

--- app/Http/Controllers/ConstanciaController.php
+++ app/Http/Controllers/ConstanciaController.php
@@ -75,6 +75,8 @@ class ConstanciaController extends Controller
             $const->diagnostico = $request->diagnostico ?? null;
             $const->tratamiento = $request->tratamiento ?? null;
             $const->seguimiento = $request->seguimiento ?? null;
+            $const->rehab = $request->rehab ?? null;
+
 
             $const->presencial = $request->presencial ?? null;
             $const->teleconsulta = $request->teleconsulta ?? null;
